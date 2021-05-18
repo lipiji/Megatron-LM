652af96f9a0f5eec1ed806c6b4b0f6506f082acf
Piji Li <pagelee.sd@gmail.com>
2021-05-18 16:25:59 +0800

    train

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,6 +1,6 @@
 python tools/preprocess_data.py \
-    --input /dockerdata/pjli/data/language_model/zhwiki/zhwiki_json.txt \
-    --output-prefix ./data/my-gptx \
+    --input /dockerdata/pjli/data/language_model/bert_newdata/data.txt \
+    --output-prefix /apdcephfs/share_916081/pjli/gptx/my-gptx \
     --vocab ./data/gptx.model \
     --dataset-impl mmap \
     --tokenizer-type GPTXTokenizer \
